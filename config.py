8da4e3f3d1e2330e230cb464534005a3f6494267
Leon <32615985+leonhess@users.noreply.github.com>
2019-05-05 23:07:06 +0200

    updated config to work with jenkins build

--- config.py
+++ config.py
@@ -1,9 +1,26 @@
-dht22 = [
-    {
-      "pin": 17,
-      "name": "dht1"
-    }
-  ]
+import socket
+
+if socket.gethostname is "leon-pi-zero-1":
+    dht22 = dht22 = [
+        {
+            "pin": 17,
+            "name": "window_front"
+        }
+    ]
+
+else if socket.gethostname is "leon-pi-zero-2":
+    dht22 = [
+        {
+            "pin": 17,
+            "name": "window_back"
+        },
+        {
+            "pin": 27,
+            "name": "desk"
+        }
+    ]
+
+
 
 influx_ip = "192.168.66.133"
 influx_database = "smarthome"
